fbca69ce264a36a1830b7960a130c33b8127236c
macaron23 <laurence.ciancia@gmail.com>
2023-03-20 23:10:26 +0100

    postQuest

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -13,11 +15,15 @@ const welcome = (req, res) => {
 app.get('/', welcome);
 
 const movieHandlers = require('./movieHandlers');
+const userHandlers = require('./userHandlers');
 
 app.get('/api/movies', movieHandlers.getMovies);
 app.get('/api/movies/:id', movieHandlers.getMovieById);
-app.get('/api/users', movieHandlers.getUsers);
-app.get('/api/users/:id', movieHandlers.getUsersById);
+app.get('/api/users', userHandlers.getUsers);
+app.get('/api/users/:id', userHandlers.getUsersById);
+
+app.post('/api/movies', movieHandlers.postMovie);
+app.post('/api/users', userHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -30,39 +30,25 @@ const getMovieById = (req, res) => {
     });
 };
 
-const getUsers = (req, res) => {
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
   database
-    .query('select * from users')
-    .then(([users]) => {
-      res.status(200).json(users);
+    .query(
+      'INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)',
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error retrieving data from database');
-    });
-};
-
-const getUsersById = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  database
-    .query('select * from users where id = ?', [id])
-    .then(([users]) => {
-      if (users[0] != null) {
-        res.status(200).json(users[0]);
-      } else {
-        res.status(404).send('Not Found');
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error retrieving data from database');
+      res.status(500).send('Error saving the movie');
     });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
-  getUsers,
-  getUsersById,
+  postMovie,
 };
